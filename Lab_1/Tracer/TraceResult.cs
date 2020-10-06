8186b2aa6888857d84e6116eb78827845f80c2a3
ListratenkoIgor <listrik_individum@mail.ru>
2020-10-06 03:02:56 +0300

    Add Tracer

---
+++ Lab_1/Tracer/TraceResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Listsoft
+{
+    namespace Lab_Tracer
+    {
+        public class MethodInfo
+        {
+            public string name;
+            public string className;
+            public long time;
+            public List<MethodInfo> methods = new List<MethodInfo>();
+        }
+        public class ThreadInfo
+        {
+            public long time;
+            public List<MethodInfo> methods = new List<MethodInfo>();
+        }
+        public class TraceResult
+        {
+            public ConcurrentDictionary<int, ThreadInfo> threads =
+                new ConcurrentDictionary<int, ThreadInfo>();
+        }
+    }
+}
