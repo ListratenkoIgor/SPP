eee19605fabd7ea0743348e808c834107ebbaf2f
ListratenkoIgor <listrik_individum@mail.ru>
2020-10-30 00:04:15 +0300

    Add Faker

---
+++ Lab_2/Faker/DTOAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Listsoft
+{
+	namespace Lab_Faker
+	{
+
+		[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+		public class DTOAttribute : Attribute
+		{
+		}
+	}
+}
---
+++ Lab_2/Faker/IFaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listsoft
+{
+    namespace Lab_Faker
+    {
+		public interface IFaker
+		{
+			T Create<T>();
+		}
+	}
+}
