13e94707049f512272ad7c5f088078410281912f
ListratenkoIgor <alastor_rujichka@mail.ru>
2020-12-30 16:04:04 +0300

    Add AssemblyBrowser

---
+++ Lab_3/WPFAssemblyBrowser/Model.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Listsoft.Lab3_AssemblyReader;
+
+namespace WPFAssemblyBrowser
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string _assemblyFile;
+        private ObservableCollection<AssemblyInfo> _assemblyInfo;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string AssemblyFile
+        {
+            get => _assemblyFile;
+            set
+            {
+                _assemblyFile = value;
+                OnPropertyChanged("AssemblyFile");
+            }
+        }
+        public ObservableCollection<AssemblyInfo> AssemblyInfo
+        {
+            get => _assemblyInfo;
+            set
+            {
+                _assemblyInfo = value;
+                OnPropertyChanged("AssemblyInfo");
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Lab_3/WPFAssemblyBrowser/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Listsoft.Lab3_AssemblyReader;
+using Microsoft.Win32;
+
+namespace WPFAssemblyBrowser
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Model _model;
+        private ICommand _openCommand;
+        public ViewModel(Model model)
+        {
+            _model = model;
+        }
+        public string AssemblyFile
+        {
+            get => _model.AssemblyFile;
+            set
+            {
+                _model.AssemblyFile = value;
+                AssemblyInfo = new ObservableCollection<AssemblyInfo>
+                {
+                    AssemblyReader.GetAssemblyInfo(_model.AssemblyFile)
+                };
+                OnPropertyChanged("AssemblyFile");
+            }
+        }
+        public ObservableCollection<AssemblyInfo> AssemblyInfo
+        {
+            get => _model.AssemblyInfo;
+            set
+            {
+                _model.AssemblyInfo = value;
+                OnPropertyChanged("AssemblyInfo");
+            }
+        }
+        public ICommand OpenCommand
+        {
+            get
+            {
+                return _openCommand ??= new RelayCommand(obj =>
+                {
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        AssemblyFile = openFileDialog.FileName;
+                    }
+                });
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
